b8c308f11567a0740397e21a4ee04905d54b4809
Rafael Sene <rafaelssene@gmail.com>
2019-08-23 15:01:15 -0300

    implementando eslint, prettier and root-import

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,7 +1,9 @@
 module.exports = (req, res, next) => {
   const [, dev] = req.headers.authorization.split(' ');
-  if(dev !== 'dev'){
-    return res.status(401).json({error: {message:"unauthorized user, necessary to pass Bearer dev"}})
+  if (dev !== 'dev') {
+    return res.status(401).json({
+      error: { message: 'unauthorized user, necessary to pass Bearer dev' },
+    });
   }
-  next()
-}
+  next();
+};
