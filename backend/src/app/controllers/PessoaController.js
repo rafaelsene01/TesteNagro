2c00fefb19d4101b477747fba15a52c2b1e49b78
Rafael Sene <rafaelssene@gmail.com>
2019-08-26 14:18:48 -0300

    implemented people route, get and post.

--- backend/src/app/controllers/PessoaController.js
+++ backend/src/app/controllers/PessoaController.js
@@ -1,10 +1,37 @@
+import * as Yup from 'yup';
+import Pessoa from '../models/Pessoa';
+
 class PessoaController {
   async index(req, res) {
-    return res.json({ ok: 'Ok' });
+    const { page = 1 } = req.query;
+
+    const pessoas = await Pessoa.findAll({
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'name', 'cpf'],
+    });
+    return res.json(pessoas);
   }
 
   async store(req, res) {
-    return res.json({ ok: 'Ok' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      cpf: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const pessoaExists = await Pessoa.findOne({ where: { cpf: req.body.cpf } });
+
+    if (pessoaExists) {
+      return res.status(400).json({ error: 'Pessoa already exists.' });
+    }
+
+    const { id, name, cpf } = await Pessoa.create(req.body);
+
+    return res.json({ id, name, cpf });
   }
 }
 
